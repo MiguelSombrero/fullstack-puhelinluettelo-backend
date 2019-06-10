54c546afe4cf04f89e3fcf713737a828f1c42e24
Miika Somero <miika.somero@gmail.com>
2019-06-10 15:17:06 +0300

    frontend samaan projektiin

---
+++ README.md
@@ -0,0 +1,9 @@
+# Full Stack Open 2019 - osa3
+
+Tämä on Full Stack Open 2019 -kurssin osa3 tehtäviä varten muodostettu repositorio.
+
+## Linkki puhelinluettelo-backend sovellukseen:
+
+https://blooming-journey-18989.herokuapp.com/
+
+(polku: /api/persons/ ...)
---
+++ build/precache-manifest.747ee9a84483ca82b695ae4723c49583.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ebf97e080a63d376a4c7776143213a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b89d538b522ea8992c2",
+    "url": "/static/css/main.571479c9.chunk.css"
+  },
+  {
+    "revision": "1a95d5a327b9fa556408",
+    "url": "/static/js/2.9fde0a89.chunk.js"
+  },
+  {
+    "revision": "0b89d538b522ea8992c2",
+    "url": "/static/js/main.fc641a73.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
